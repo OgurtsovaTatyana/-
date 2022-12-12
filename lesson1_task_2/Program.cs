70570f2f6d3c1ed444fb2a1a8845548d5e7d76a4
Tatyana2022 <zetaal@mail.ru>
2022-12-12 20:39:25 +0700

    add lesson1_task_2

---
+++ lesson1_task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.WriteLine("введите 2 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a>b)
+{Console.WriteLine(a);}
+else 
+{Console.WriteLine(b);}
