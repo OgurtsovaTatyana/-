51c54955934e7311a824b01ca167d935271a3593
Tatyana2022 <zetaal@mail.ru>
2022-12-12 22:00:31 +0700

    add lesson1_task_8

---
+++ lesson1_task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine());
+int count =2;
+while (count<=number) {
+Console.WriteLine(count);
+count=count+2;
+}
+
