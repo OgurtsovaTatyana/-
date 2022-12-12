749b08f96eeaf52ae66cfd41ceb60cec87b40c50
Tatyana2022 <zetaal@mail.ru>
2022-12-12 21:48:59 +0700

    add lesson1_task_6

---
+++ lesson1_task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine());
+if (number%2>0)
+{Console.WriteLine("число нечетное");}
+else 
+{
+ {Console.WriteLine("число четное");}   
+};
