9dc384f57e356fd2e04608cf53ad5585684443a2
Tatyana2022 <zetaal@mail.ru>
2022-12-12 21:36:20 +0700

    add lesson1_task_4

---
+++ lesson1_task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите 3 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+if (a>b & a>c)
+{Console.WriteLine(a);};
+ if (b>a & b>c) 
+ {Console.WriteLine(b);};
+ if (c>a & c>b) 
+ {Console.WriteLine(c);};
