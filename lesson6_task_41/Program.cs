5ccfc22bb2305a45bebbbc641681f2bcdbfdfa3d
Tatyana2022 <zetaal@mail.ru>
2022-12-20 00:27:38 +0700

    added Lesson6_task 41,43

---
+++ lesson6_task_41/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите элементы массива через пробел");
+string[] sArray = (Console.ReadLine()).Split(' ');
+int size = sArray.Length;
+int[] nArray = new int[size];
+int[] GetArray(string[] array)
+{
+    int i = 0;
+    int[] newArray = new int[size];
+    foreach (string elem in array)
+    {
+        newArray[i] = int.Parse(elem);
+        i++;
+    }
+   // Console.WriteLine("massiv " + string.Join(" ", newArray));
+    return newArray;
+    
+};
+
+void CheckArray(int[] array)
+{
+    int count = 0;
+    foreach (int elem in array)
+    {
+        if (elem > 0) count++;
+    };
+    Console.WriteLine("Количество положительных чисел: " + count);
+};
+nArray = GetArray(sArray);
+CheckArray(nArray);
---
+++ lesson6_task_43/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите параметры уравнений (через пробел): b1, k1, b2 ,k2");
+string[] sArray = (Console.ReadLine()).Split(' ');
+double[] nArray = new double[sArray.Length];
+int i = 0;
+foreach (string elem in sArray)
+{
+    nArray[i] = double.Parse(elem);
+    i++;
+}
+if (nArray[1] == nArray[3]) 
+    { Console.WriteLine("точек пересечения нет"); }
+else
+{
+    double x = (nArray[0] - nArray[2]) / (nArray[3] - nArray[1]);//x=(b1-b2)/(k2-k1)
+    double y = (nArray[1] * (nArray[2] - nArray[0])) / (nArray[3] - nArray[1]) + nArray[0]; //(y=k1*(b1-b2)/(k2-k1)+b1
+    Console.WriteLine($"коордитаны точки пересечения прямых y={nArray[1]}*x + {nArray[0]} и y={nArray[3]}*x + {nArray[2]} равны {x},{y}");
+}
+
+ //Вариант2
+ /*Console.WriteLine("введите параметры уравнений (через enter): b1, k1, b2 ,k2");
+ double b1= double.Parse(Console.ReadLine());
+double k1= double.Parse(Console.ReadLine());
+double b2= double.Parse(Console.ReadLine());
+double k2= double.Parse(Console.ReadLine());
+
+void FindDot ( double b1,  double k1, double b2 , double k2){
+if (k1==k2) 
+    { Console.WriteLine("точек пересечения нет");}
+else
+{
+    double x=(b1-b2)/(k2-k1);
+    double y=b1+(k1*(b1-b2))/(k2-k1);
+    Console.WriteLine($"коордитаны точки пересечения прямых y={k1}*x + {b1} и y={k2}*x + {b2} равны {x},{y}");
+}
+}
+FindDot(b1, k1, b2 ,k2);
+*/
