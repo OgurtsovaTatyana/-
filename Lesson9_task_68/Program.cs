f59cb619a1a777a2e81e39c6945432eba1a34496
Tatyana2022 <zetaal@mail.ru>
2022-12-31 21:14:04 +0700

    add lesson9 tasks 64,66,68

---
+++ Lesson9_task_68/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+Console.WriteLine("введите N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("введите M");
+int m = int.Parse(Console.ReadLine());
+
+int FuncAk(int numN, int numM)
+{
+    if (numM == 0) { return numN + 1; }
+    else
+    {
+        if (numM > 0 & numN == 0) { return FuncAk(numM - 1, 1); }
+        
+        else
+        {
+            if (numM > 0 & numN > 0) return FuncAk(numM - 1, FuncAk(numM, numN - 1));
+        }
+    }
+
+
+};
+Console.WriteLine(FuncAk(n, m));
---
+++ lesson9_task_66/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.WriteLine("введите N");
+int n=int.Parse(Console.ReadLine());
+Console.WriteLine("введите M");
+int m=int.Parse(Console.ReadLine());
+
+int SumNM(int numN, int numM) {
+    if (numN<=numM) {
+        return SumNM(numN+1,numM)+numN;
+} else return 0;
+}
+Console.WriteLine(SumNM(n,m));
+
